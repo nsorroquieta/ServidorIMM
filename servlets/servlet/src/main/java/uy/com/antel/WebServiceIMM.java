90301cfff569c45d73236723d3d6a3bafe6d48c0
Goday Soria, José Leonel <jgodaysoria@antel.com.uy>
2018-10-12 10:44:46 -0300

    Se agregó función de Comprar ticket en el webservice

--- servlets/servlet/src/main/java/uy/com/antel/WebServiceIMM.java
+++ servlets/servlet/src/main/java/uy/com/antel/WebServiceIMM.java
@@ -1,12 +1,24 @@
 package uy.com.antel;
 
 import javax.jws.WebService;
+import java.util.Date;
 
 @WebService
 public class WebServiceIMM {
 
     public String test(String inputText)
     {
+        Date fechayhora = new Date();
+        Date fechayhora2 = new Date();
+        Ticket ticket = new Ticket("MAA1020", fechayhora, fechayhora2, 10);
+
         return "Respues de WebServiceIMM: " + inputText;
     }
+
+    public String comprarTicket(int AgenciaVenta, String carResgistry, String salesDateTime, String startDateTime, int minutes){
+        int precioMinutos = 60;
+        int total = minutes * precioMinutos;
+        return "El precio a pagar es: "+total;
+    }
+
 }
